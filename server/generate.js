ba48f204304ad9057f053734b0c45f0c40e78c73
Phoenix0415 <phoenixx0415@gmail.com>
2024-05-02 00:06:01 +0930

    update backend: generate.js. use gpt-4 as the llm model

--- server/generate.js
+++ server/generate.js
@@ -1,20 +1,25 @@
 import openaiClient from "./api.js";
 
-const MODEL = "gpt-4";
 const generate = async (queryDescription) => {
+
+  const chatGPT = async (queryDescription) => {
+    const message = [
+      { role: "system", content: `You are a translator from plain English to SQL.` },
+      { role: "user", content: `Convert the following natural language description into a SQL query:\n\nShow all all the elements in the table users` },
+      { role: "assistant", content: "SELECT * FROM users;" },
+      { role: "user", content: `Convert the following natural language description into a SQL query:\n\n${queryDescription}` },
+    ];
     const response = await openaiClient.chat.completions.create({
-        model: MODEL,
-        messages: [{
-            role: 'system',
-            content: 'You are a helpful assistant.'
-        }, {
-            role: 'user',
-            content: `Convert the following natural language description into a SQL query:\n\n${queryDescription}.`
-        }],
-        max_tokens: 100,
-        temperature: 0
+      model: "gpt-4",
+      messages: message,
     });
+
     return response.choices[0].message.content;
+  }
+
+  const sqlQuery = await chatGPT(queryDescription);
+  return sqlQuery;
+
 };
 
 export default generate;
